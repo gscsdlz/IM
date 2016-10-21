7e4a60fe5f6747675aad16d183a8b1f71dbae76f
Daemon <lz842063523@foxmail.com>
2016-10-21 20:21:30 +0800

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -3,11 +3,14 @@
 #include <pthread.h>
 #include <sys/socket.h>
 //#include <mysql/mysql.h>
+#include <map>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <list>
+
+#define BUF_SIZE 1024
 
 using namespace std;
 
@@ -43,7 +46,8 @@ private:
     list<int>userSocket;  //store the client's socket;
     int gid;
 };
-map<int, Group *>groupMap;
+map<int, Group *>groupMap;  //int mains the gid
+map<int, int>socketMap;  //First int mains socket, second int mains gid;
 
 void error(char *message)
 {
@@ -52,14 +56,14 @@ void error(char *message)
     exit(1);
 }
 
-void init(int port, int maxClnt)
+int init(int port, int maxClnt)
 {
     int serv_sock;
     struct sockaddr_in serv_adr;
 
 
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    memset(&serv_addr, 0, sizeof(serv_adr));
+    memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_adr.sin_port = htons(port);
@@ -75,50 +79,62 @@ void init(int port, int maxClnt)
 
     }
     pthread_mutex_init(&mtx, NULL);
+    return serv_sock;
 }
 
 void sendInfo(char *message, int clnt_sock)
 {
-    if(m[clnt_sock] != NULL)
-        int gid = m[clnt_sock]->sendInfo(message[]);
-
+    int gid = socketMap[clnt_sock];
+    if(gid == 0)
+    {
+        char errorMsg[] = "No such group!\n";
+        write(clnt_sock, errorMsg, sizeof(errorMsg));
+    }
+    else
+    {
+        groupMap[gid]->sendInfo(message);
+    }
 }
 
 void login(char *message, int clnt_sock)
 {
-    //message type Lusername:password
+    //message type Lusername:password:gid
     int len = strlen(message);
     char username[100];
     char password[100];
-    int p1 = 0, p2 = 0;
-    bool flag = false;
+    char gidString[100];
+    int p1 = 0, p2 = 0, p3 = 0;
+    memset(username, '\0', sizeof(username));
+    memset(password, '\0', sizeof(password));
+    memset(gidString, '\0', sizeof(gidString));
+
+    int flag = 0;
     for(int i = 1; i < len; ++i)
     {
         if(message[i] == ':')
         {
-            flag = true;
+            flag++;
             continue;
         }
-        if(flag)
+        if(flag == 0)
+            username[p1++] = message[i];
+        else if(flag == 1)
             password[p2++] = message[i];
         else
-            username[p1++] = message[i];
+            gidString[p3++] = message[i];
     }
+
     /////////////////////////////
     //mysql SELECT;
     ////////////////////////////
-    int gid = 1;  //from Database
-    if(groupMap[gid] == NULL)
-        groupMap[gid] = new Group();
+    int gid = atoi(gidString);  //from Database
 
     pthread_mutex_lock(&mtx);
+    socketMap[clnt_sock] = gid;
+    if(groupMap[gid] == NULL)
+        groupMap[gid] = new Group();
     groupMap[gid]->join(clnt_sock);
     pthread_mutex_unlock(&mtx);
-}
-
-void sendMsg(char * message, int clnt_sock)
-{
-    write(clnt_sock, message, strlen(message));
 }
 
 void * readInfo(void *arg)
@@ -127,29 +143,36 @@ void * readInfo(void *arg)
     int str_len = 0;
     char msgBuff[BUF_SIZE];
     memset(msgBuff, 0, sizeof(msgBuff));
+    int recv_len;
 
-    do
+    while((recv_len = read(clnt_sock, &msgBuff[str_len], BUF_SIZE)) != 0)
     {
-        int recv_len = read(clnt_sock, &msgBuff[str_len], BUF_SIZE - str_len);
         if(recv_len == -1)
             error("read() error!");
         str_len += recv_len;
-    }
-    while(msgBuff[str_len - 1] != '\0');
+        if(msgBuff[str_len - 1] == '\0') //recv all the info
+        {
+            if(msgBuff[0] == 'L') // mains Login
+                login(msgBuff, clnt_sock);
+            else if(msgBuff[0] == 'S')
+                sendInfo(msgBuff, clnt_sock);
 
-    if(msgBuff[0] == 'L') // mains Login
-        login(msgBuff, clnt_sock);
-    else if(msgBuff[0] == 'S')
-        sendMsg(msgBuff);
+            memset(msgBuff, '\0', sizeof(msgBuff));
+            str_len = 0;
+        }
+    }
+    if(socketMap[clnt_sock] != 0)
+        groupMap[socketMap[clnt_sock]]->leave(clnt_sock);
+    close(clnt_sock);
 }
 
-void keepConnect()
+void keepConnect(int serv_sock)
 {
     struct sockaddr_in clnt_adr;
     pthread_t tid;
     while(1)
     {
-        int clnt_adr_sz = sizeof(clnt_adr);
+        socklen_t clnt_adr_sz = sizeof(clnt_adr);
         int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
         pthread_create(&tid, NULL, readInfo, (void *)&clnt_sock);
         pthread_detach(tid);
@@ -164,7 +187,7 @@ int main(int argc, char *argv[])
     int maxClnt = atoi(argv[2]);
     if(argc == 4 && argv[3][0] == 'D')
         debug = true;
-    init(port, maxClnt);
-
+    int serv_sock = init(port, maxClnt);
+    keepConnect(serv_sock);
     return 0;
 }
